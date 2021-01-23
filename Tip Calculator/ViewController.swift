ad19dd0316090bffb4c3b5ae6082dd244a432d3c
jonriv48376 <58375544+jonriv48376@users.noreply.github.com>
2021-01-23 12:21:17 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Jonathan Rivero on 1/23/21.
+//  Copyright © 2021 Jonathan Rivero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
